569b7e244d8914a3b86fb10c1e68aa39a965d502
hicham <ELmliki.hicham@gmail.com>
2021-12-21 03:29:39 -0800

    modif

--- realisation/calculatrice.js
+++ realisation/calculatrice.js
@@ -1,20 +1,86 @@
-var btm 
-var output
-var a 
-var b 
-var c 
-
-
-a=1
-b=2
-c=3
-
-btm = document.getElementById("submit")
-
-function afficher(){
+// Variables
+var x,y,solution,operation;
+var xString, yString;
 
 
 
+// Métier
+function Calculer(x,y,operation){
+    var _solution = undefined;
+    switch (operation) {
+        case '+':
+            _solution = x + y;
+            break;
+        case '-':
+            _solution = x-y;
+                break;
+        default:
+            break;
+    }
+    return _solution;
+}
 
 
+// Présentation
+function ClickNumber(number){
+    if(x == undefined){
+        if(xString == undefined) xString = '';
+        xString += number;
+    } 
+    else {
+        if(yString == undefined) yString = '';
+        yString += number;
+    } 
+
+    Afficher();
+}
+
+function Afficher(number){
+
+    var afficheur = document.getElementById("afficheur");
+    afficheur.value = "";
+    
+    if(x != undefined && y != undefined && operation != undefined){
+
+        afficheur.value = number;
+    }else{
+        if(xString != undefined)
+        afficheur.value += xString 
+        if(operation != undefined)
+            afficheur.value += operation     
+        if(yString != undefined)
+            afficheur.value += yString 
+
+        }
+ 
+   
+}
+
+
+function Operation(operationParam){
+    if(operation == undefined){
+        operation = operationParam;
+        x = parseFloat(xString);
+        Afficher();
+    }else{
+        alert("Vous avez déjà choisi l'opération " + operation);
+    }
+}
+
+function Egale(){
+  
+    y = parseFloat(yString);
+    solution = Calculer(x,y,operation);
+    Afficher(solution);
+}
+
+function Init(){
+    x = undefined;
+    y = undefined;
+    xString = undefined;
+    yString = undefined;
+    operation = undefined;
+    var afficheur = document.getElementById("afficheur");
+    
+    afficheur.value = "";
 }
