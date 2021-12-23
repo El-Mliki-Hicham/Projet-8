9c89dd8bf9a87b542bb0f824de17c515b59db44b
hicham <ELmliki.hicham@gmail.com>
2021-12-23 01:40:53 -0800

    modif

--- liveconding-1/ex.js
+++ liveconding-1/ex.js
@@ -1,76 +1,53 @@
-var a,b,aString,bString,operation,resulta ;
+var a , b , operation, afficher , aString , bString  
 
+function oncklikNumbre(number){
+if(aString == undefined){ aString = number}
+else (bString = number)
 
-
-function oncklikNumbre(number) {
-if (aString == undefined)
-{aString = number }
-else {bString = number}     
 afficher()
+    
 }
-
-
-
 
 function afficher(number) {
-var afficher = document.getElementById("afficher")
-afficher.value = ""
+    var afficher = document.getElementById("afficher")
+    afficher.value = ""
 
-if (a!=undefined && b!=undefined && operation !=undefined ) {afficher.value = number}
-else{
-if (aString != undefined) {afficher.value += aString }
-if (operation != undefined) {afficher.value += operation }
-if (bString != undefined) {afficher.value += bString }
+    if ( a != undefined && b != undefined && operation != undefined )
+    {afficher.value = number }
+    else{
+        if(aString != undefined){afficher.value += aString}
+        if ( operation != undefined){afficher.value += operation}
+        if(bString != undefined){afficher.value += bString}
 
-
-}
-
-afficher()
-
-}
-
-function Operation(operationParam) {
-    if (operation == undefined ) 
-    {operation = operationParam}
-    afficher()
-}
-
-
-
-
-
-
-function egaLe() {
-
-a= parseFloat(aString)
-b= parseFloat(bString)
-
-var resulta = calcule(a,b,operation)
-
-afficher(resulta)    
-}
-
-
-function calcule(a,b,operation) {
- resulta == undefined
-
- switch (operation) {
-     case "+":
-         resulta = a+b
-         break;
- 
-     default:
-         break;
-
-        }
-
-return resulta 
     }
 
+}
+    function operaTion(operationParam) {
 
-
+        if (operation == undefined) 
+        { operation = operationParam  }
+        afficher ()
+    }
     
+function egaLe() {
 
+    a= parseFloat(aString)
+    b= parseFloat(bString)
 
+     resulta = calcul (a,b,operation )
+     afficher (resulta)  
+}
 
-  
+function calcul (a,b,operation) {
+   resulta = undefined 
+
+   switch (operation ) {
+       case "+":
+           resulta= a+b
+           break;
+   
+       default:
+           break;
+   }
+   return resulta
+}
